59488f4d603345c2f0e8fe08407acc2b0aeabbd2
github username <github email address>
2017-05-02 12:29:43 -0400

    Added parallax

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ export default class App extends Component {
   render() {
     return (
       <div>
+
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Little Big Giant</h2>
         </div>
+        <div className="parallax">
+          <div className="reveal">
+            <div className="reveal curve">
+              <a href="https://github.com/lgrapstein/bear">Github</a>
+            </div>
 
-        <div className="reveal">
-          <div className="reveal curve">
-            <a href="https://github.com/lgrapstein/bear">Github</a>
-          </div>
+            <div className="reveal plug">
+              <a href="https://github.com/lgrapstein/bear">Github</a>
+            </div>
 
-          <div className="reveal plug">
-            <a href="https://github.com/lgrapstein/bear">Github</a>
-          </div>
-
-          <div className="reveal paper">
-            <a href="https://github.com/lgrapstein/bear">Github</a>
+            <div className="reveal paper">
+              <a href="https://github.com/lgrapstein/bear">Github</a>
+            </div>
           </div>
         </div>
-
-
       </div>
     )
   }
