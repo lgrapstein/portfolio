4ed4fc028426c5807d0e4b7adcec8763196307a8
github username <github email address>
2017-05-13 18:59:10 -0400

    Added background, fixed transitions, added links, added music

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import LittleBigGiant from './LittleBigGiant.png';
-import './App.css';
+import React, { Component } from 'react'
+import LittleBigGiant from '../public/images/logo/LittleBigGiant.png'
+import './App.css'
+import Music from './components/music.jsx'
+
 
 export default class App extends Component {
+
   render() {
     return (
-      <div>
-
-        <div className="App-header">
-          <img src={LittleBigGiant} className="App-logo" alt="logo" />
-          <h2>Little Big Giant</h2>
-        </div>
+      <div className="App">
+        <img className="App-header" src={LittleBigGiant} alt="logo"/>
+        <Music />
         <div className="parallax">
-            <div className="reveal curve">
-              <a href="https://github.com/lgrapstein/bear">Github</a>
-            </div>
+          <div className="reveal travelshare display-flex">
+            <a className="flex-auto" href="https://github.com/lgrapstein/travel-share" target="_blank">Github</a>
+            <a className="flex-auto" href="http://travel-share.herokuapp.com/" target="_blank">App</a>
+          </div>
 
-            <div className="reveal plug">
-              <a href="https://github.com/lgrapstein/bear">Github</a>
-            </div>
+          <div className="reveal bear display-flex">
+            <a className="flex-auto" href="https://github.com/lgrapstein/bear" target="_blank">Github</a>
+            <a className="flex-auto" href="http://beargame.surge.sh/home" target="_blank">App</a>
+          </div>
 
-            <div className="reveal paper">
-              <a href="https://github.com/lgrapstein/bear">Github</a>
-            </div>
+          <div className="reveal wifi display-flex">
+            <a className="flex-auto" href="https://github.com/lgrapstein/nyc-wifi-2" target="_blank">Github</a>
+            <a className="flex-auto" href="https://wifi-freerider.herokuapp.com/" target="_blank">App</a>
           </div>
         </div>
+      </div>
     )
   }
 }
---
+++ src/components/music.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import * as d3 from "d3";
+import '../../public/style.css'
+import byebyemac from '../../public/clips/byebyemac.wav'
+
+export default class Music extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      audio: 'play',
+    }
+
+    this.audio = document.getElementsByTagName("audio")[0]
+  }
+
+  componentDidMount() {
+    var audioElements = document.getElementsByTagName("audio")
+    this.byebyemac = audioElements[0]
+    this.byebyemac.volume = .8
+    this.byebyemac.play()
+  }
+
+  toggleActive() {
+    this.setState({
+      audio: this.state.audio,
+    })
+  }
+
+
+  render() {
+    return (
+      <div className="container">
+        <audio><source src={byebyemac}></source></audio>
+
+        <div className="player">
+
+
+        </div>
+      </div>
+    )
+  }
+}
